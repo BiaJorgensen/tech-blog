5e02476d3fec1264c1430db2eac35dc74c4bac93
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-19 14:52:26 -0500

    Added post model

---
+++ Develop/models/Post.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username_id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    },
+    date_created: {
+        type: DataTypes.DATEONLY,
+        allowNull: false
+      },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+    createdAt: 'date_created',
+    updatedAt: false
+  }
+);
+
+module.exports = Post;
