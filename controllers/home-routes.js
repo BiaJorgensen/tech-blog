d5264da4285891ff3bf0726f343bf2561b3f1c80
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-21 12:43:43 -0500

    Added dashboard route; handlebars; and withAuth to verify if user is logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
@@ -16,6 +17,23 @@ router.get('/', async (req, res) => {
         const posts = postsData.map((post) => post.get({ plain: true }));
 
         res.render('homepage', {posts});
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userPosts = await User.findByPk(req.session.user_id,{
+            attributes: { exclude: ['password'] },
+            include: [{model: Post}],
+        });
+
+
+        const posts = userPosts.map((post) => post.get({ plain: true }));
+
+        res.render('dashboard', {posts});
 
     } catch (err) {
         res.status(500).json(err);
