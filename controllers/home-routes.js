ac444f60ed46f2378ef16f5ed0969a2432f0d4c1
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-21 16:09:34 -0500

    Created login.handlebars; added login get route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -38,6 +38,17 @@ console.log(posts);
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
