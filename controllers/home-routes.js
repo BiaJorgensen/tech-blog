3cb0c103d2c5dfe95653329d8a62b65fbcd3076c
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-21 12:03:50 -0500

    Fixed res.render in home-routes.js; added secret to .env; added boilerplate to server.js; exporting home-routes though index.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post, User } = require('../models');
 
-router.get('./', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const postsData = await Post.findAll({
             include: [
@@ -13,11 +13,13 @@ router.get('./', async (req, res) => {
         });
 
 
-        const posts = postsData.map((post) => post.get({ plain: true}));
+        const posts = postsData.map((post) => post.get({ plain: true }));
 
-        res.render('homepage', posts);
+        res.render('homepage', {posts});
 
     } catch (err) {
         res.status(500).json(err);
     }
 })
+
+module.exports = router;
