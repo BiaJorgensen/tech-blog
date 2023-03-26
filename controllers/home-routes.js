c1cafa624457b6beefb02b6e910bd25f53049531
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-26 13:50:36 -0500

    Changed href for posts that are in the homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -54,14 +54,14 @@ router.get('/new-post', withAuth, (req, res) => res.render('newPost', {loggedIn:
 router.get('/post/:id', withAuth, async (req, res) => {
 
     try {
-        const onePostDate = await Post.findByPk(req.params.id)
-        if (!onePostDate) {
+        const onePostData = await Post.findByPk(req.params.id)
+        if (!onePostData) {
             res.status(404).json({ message: 'No posts found with that ID'});
           return;
         }
        
 
-        const userPost = onePostDate.get({ plain: true})
+        const userPost = onePostData.get({ plain: true})
 
         if (userPost.username_id == req.session.user_id) {
             res.render('updatePost', {
