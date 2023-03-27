6780ac18dc231f3f6bcbae78be8dbef9e02d0422
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-27 17:56:30 -0500

    Added option to see all comments

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,23 +2,39 @@ const addComment = async (event) => {
     event.preventDefault();
     const post_id = window.location.pathname.split('/').pop();
     const content = $('#newComment').val();
-   
 
     if (content) {
-    const response = await fetch('/api/comment', {
-    method: 'POST',
-    body: JSON.stringify({ content, post_id}),
-    headers: { 'Content-Type': 'application/json' }
+        const response = await fetch('/api/comment', {
+        method: 'POST',
+        body: JSON.stringify({ content, post_id}),
+        headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace(`/comment/${post_id}`);
+        }
+        else {
+            alert("Please add a comment")
+        }
+    }
+};
+
+const seePostComments = async (event) => {
+    event.preventDefault();
+    const post_id = window.location.pathname.split('/').pop();
+    
+    const response = await fetch(`/comment/${post_id}`, {
+    method: 'GET',
     });
 
     if (response.ok) {
         document.location.replace(`/comment/${post_id}`);
     }
     else {
-        alert("Please add a comment")
-    }
-    
-    }
+        alert(response.statusText)
+    } 
 };
 
+
 $('#add-comment-btn').click(addComment);
+$('#see-all-comments').click(seePostComments);
