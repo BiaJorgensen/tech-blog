cc17c1a8eb86b207cfcfd4f191184c61ef3ead4a
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-03-26 23:06:21 -0500

    Created post route to add comment to post; Created fetch request to post comment; Added comment.js to comments.handlebars

---
+++ public/js/comment.js
@@ -0,0 +1,24 @@
+const addComment = async (event) => {
+    event.preventDefault();
+    const post_id = window.location.pathname.split('/').pop();
+    const content = $('#newComment').val();
+   
+
+    if (content) {
+    const response = await fetch('/api/comment', {
+    method: 'POST',
+    body: JSON.stringify({ content, post_id}),
+    headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    }
+    else {
+        alert("Please add a comment")
+    }
+    
+    }
+};
+
+$('#add-comment-btn').click(addComment);
